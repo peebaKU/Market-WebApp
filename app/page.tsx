3b21605ad02e638ce7d9cb967d48c56e6f2764bf
peebaKU <baramee.254450@gmail.com>
2024-05-02 23:55:57 +0700

    add function ButtonWrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
+'use client';
 import AllProducts from "@/components/AllProducts";
 import Custom from "@/components/Custom";
 import Student from "@/components/Student";
+
+const ButtonWrapper = ({children})=>{
+  return(
+    <div className="w-32 h-8 mx-2 text-center text-while bg-blue-500 hover:bg-blue-600 hover:cursor-pointer active:bg-red-500 rounded-md">
+      {children}
+    </div>
+  )
+}
+
 export default function Home() {
+  const clickHandler = (data)=>{
+    alert(`your number is ${data}`)
+  }
   return (
     <div className="flex flex-col items-center justify-start min-h-screen min-w-full mx-auto">
-      <div className="text-3xl text-blue-500 p-3 m-3">
-        All Products List
-      </div>
-      <AllProducts/>
+      <ButtonWrapper>
+          <button className="w-full h-full" onClick={clickHandler.bind(this, 1)}>1</button>
+      </ButtonWrapper>
     </div>
   );
 }
